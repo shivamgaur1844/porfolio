0f7774cc70597010a9de6564019301aefeb60ae4
shivamgaur1844 <168626026+shivamgaur1844@users.noreply.github.com>
2025-07-16 00:14:34 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/p.JPG';
+import profileImage from '../../assets/p.jpg';
 
 const About = () => {
   return (
